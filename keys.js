5e122a9321c6c74070557eec9a2fe02112e6f508
efrencav <efren@abstract-creative.com>
2018-10-20 21:56:18 -0500

    new files

--- keys.js
+++ keys.js
@@ -1,29 +1,17 @@
 console.log('this is loaded');
 
 // Spotify key information
-var Spotify = require('node-spotify-api');
- 
-var spotify = new Spotify({
-  id: "282f834858e84faab5e1813afb6611ac",
-  secret: "6fc5a6cc2ab64a60a2a6c7723b744d32"
-});
- 
-spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
-  }
- 
-console.log(data); 
-});
-
-
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
 
-// request('http://www.google.com', function (error, response, body) {
-//   console.log('error:', error); // Print the error if one occurred
-//   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-//   console.log('body:', body); // Print the HTML for the Google homepage.
-// });
+// OMDB information
+exports.OMDB = {
+  id: process.env.OMDB_KEY
+}
+
+
+
+
+
--- liri.js
+++ liri.js
@@ -1,28 +1,147 @@
 require("dotenv").config();
+// Keys JS file
+const keys = require('./keys');
+// Request details
+const request = require('request');
+// Inquirer details
+const inquirer = require('inquirer');
+// Moment details
+const moment = require('moment');
+// Spotify details
+const Spotify = require('node-spotify-api');
+// Chalk details
+const chalk = require('chalk');
+// Spotify details
+var spotify = new Spotify(keys.spotify);
+// Inquirer prompt
+inquirer.prompt({
+    type: 'list',
+    name: 'choice',
+    message: 'Please select an option: ',
+    choices: ["Find about your favorite bands' concerts", "Get spotify info about songs you like", "Get info about any movie", "Get a demo"]
+}).then(liri => {
+    switch (true) {
+        case liri.choice === "Find about your favorite bands' concerts":
+            concertThis();
+            break;
 
-var keys = require("./keys.js"); // Grab data from keys.js
-var fs = require("fs"); // node package for reading and writing files
-var request = require('request'); // node package for making http requests
-var spotify = new Spotify(keys.spotify);  // node package that handles Spotify requests
+        case liri.choice === "Get spotify info about songs you like":
+            spotifyThisSong();
+            break;
 
-//Prompts for command line syntax
-var action = process.argv[2];
-var value = process.argv[3];
+        case liri.choice === "Get info about any movie":
+            movieThis();
+            break;
 
-//Spotify Exercise
+        case liri.choice === "Get a demo":
+            doWhatItSays();
+            break;
 
-//End Spotify Exercise
-var Spotify = require('node-spotify-api');
- 
-var spotify = new Spotify({
-  id: <your spotify client id>,
-  secret: <your spotify client secret>
+        default:
+
+    }
+    // Function for concerts
+    function concertThis() {
+        inquirer.prompt({
+            type: "input",
+            message: "Please enter an artist?",
+            name: "nameOfArtist"
+        }).then(artistSelection => {
+
+            var url = "https://rest.bandsintown.com/artists/" + artistSelection.nameOfArtist + "/events?app_id=b2b1d13e2f579627ed525e0f00cf2713";
+            console.log(url);
+
+            request(url, (err, res, body) => {
+                if (!err && res.statusCode === 200) {
+                    var obj = JSON.parse(body);
+                    if (body === 'undefined') {
+                        console.log("Please enter a different artist.")
+                    }
+                    else {
+                        for (i = 0; i < obj.length; i++) {
+                            var newTime = obj[i].datetime;
+                            newTime = moment(newTime).format("MM/DD/YYYY");
+                            console.log(`
+==================================================
+${obj[i].lineup} EVENT NUMBER ${i}
+==================================================
+${chalk.blue.bold(`* Name of venue: ${obj[i].venue.name}
+* Location of venue: ${obj[i].venue.country}, ${obj[i].venue.city}
+* Date of event: ${newTime}\n`)}`);
+                        }
+                    }
+                }
+            });
+        });
+    }
+    // Spotify function
+    function spotifyThisSong() {
+        inquirer.prompt({
+            type: "input",
+            message: "Enter song: ",
+            name: "songName"
+        }).then(songSelection => {
+            spotify.search({ type: 'track', query: songSelection.songName, limit: 20 }, (err, body) => {
+                if (err) throw err;
+                console.log(`
+=========================================================
+INFO REGARDING THE SONG ${body.tracks.items[0].name}
+=========================================================
+${chalk.blue.bold(`* Artist: ${body.tracks.items[0].artists[0].name}
+* Name of song: ${body.tracks.items[0].name}
+* To play the song click on the following link: ${chalk.magenta.underline(`${body.tracks.items[0].preview_url}`)}
+* The song's album is called: ${body.tracks.items[0].album.name}\n`)}`);
+            });
+            // var urlQuery=`https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx`;
+            //spotify.request(urlQuery)
+            //.then(body=>{
+            //    console.log(body.album.available_markets[0]);
+            //})
+            //.catch.error(`Error occurred: ${err}`);
+            //});
+        });
+    }
+    // Movie function
+    function movieThis() {
+        inquirer.prompt({
+            type: "input",
+            message: "Please enter the movie's name: ",
+            name: "movieName"
+        }).then(movieSelection => {
+            var queryURL = "http://www.omdbapi.com/?t=" + movieSelection.movieName + "&y=&plot=short&apikey=trilogy";
+
+            request(queryURL, (err, res, body) => {
+                if (!err && res.statusCode === 200) {
+                    json = JSON.parse(body);
+                    //console.log(json);
+                    console.log(`
+============================================
+INFORMATION ABOUT ${json.Title}
+============================================
+${chalk.blue.bold(`* Year movie came out is: ${json.Year}
+* IMDB movie rating: ${json.imdbRating} 
+* Country of production: ${json.Country}
+* Movie plot: ${json.Plot}
+* Movie Actors: ${json.Actors}\n`)}`);
+                }
+            });
+        });
+
+    }
+    // doWhatItSays function
+    function doWhatItSays() {
+        spotify.search({ type: 'track', query: 'All the Small Things', limit: 20 }, (err, body) => {
+            if (err) throw err;
+            console.log("The title of the movie is : " + err);
+            console.log(body);
+            console.log(`Artist: `);
+            console.log(`Name of song: `);
+            console.log(`To play the song click on the following link: `);
+            console.log(`The song's album is `);
+        });
+
+    }
+
 });
- 
-spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
-  }
- 
-console.log(data); 
-});
+
+
